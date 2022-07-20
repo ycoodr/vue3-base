6681d108d1936254db3ddf01ea6bed09ce1a9644
ycoodr <ycoodr@gmail.com>
2022-07-20 16:51:51 +0300

    init and sample

---
+++ src/components/PeopleList.vue
@@ -0,0 +1,17 @@
+<template>
+<h1>People list</h1>
+<PeopleListItem />
+<PeopleListItem />
+<PeopleListItem />
+</template>
+
+<script>
+import PeopleListItem from './PeopleListItem';
+export default {
+    name: 'PeopleList',
+    components: {
+        PeopleListItem,
+    }
+
+}
+</script>
---
+++ src/components/PeopleListItem.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+<h3>{{ name }}</h3>
+<p>Age: {{ age }} years</p>
+<p>Hair color: {{ hairColor }}</p>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'PeopleListItem',
+    data(){
+        return {
+            name: 'Shaun Wassel',
+            age: 100,
+            hairColor: 'brown',
+        }
+    }
+}
+</script>
